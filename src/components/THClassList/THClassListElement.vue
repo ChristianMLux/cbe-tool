3d36cbfd1175f5aa214e01f22de2348fa2046707
CML <christian.m.lux@gmail.com>
2021-09-16 08:20:30 +0200

    feat: adds classlistelement component

---
+++ src/components/THClassList/THClassListElement.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="th__list-element">
+    <div class="th__list-wrapper">
+      <p class="th__student-name">{{ studentName }}</p>
+      <p class="th__student-open-issues">{{ issuesCounter }}</p>
+      <p class="th__student-commit-counter">{{ commitCounter }}</p>
+      <p class="th__student-repo-counter">{{ repoCounter }}</p>
+      <a :href="studentGitURL">GitHub-Profil</a>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "THClassListElement",
+  props: {
+    studentName: {
+      type: String,
+    },
+    issuesCounter: {
+      type: String,
+    },
+    commitCounter: {
+      type: String,
+    },
+    repoCounter: {
+      type: String,
+    },
+    studentGitURL: {
+      type: String,
+    },
+  },
+};
+</script>
