1479e92b7c844a34546846d27c99f8fe7adc3898
CML <christian.m.lux@gmail.com>
2021-10-26 14:45:46 +0200

    feat: refactors the lesson recording list and adds basic styling

--- src/components/LRRecordingList/LRListElement.vue
+++ src/components/LRRecordingList/LRListElement.vue
@@ -1,9 +1,10 @@
 <template>
-  <li>
-    <div class="lr__element-wrapper">
-      <p>{{ date }}</p>
-      <p>{{ topic }}</p>
-      <a :href="playURL">Play</a>
+  <li class="lr__element-wrapper">
+    <p class="lesson-topic">{{ topic }}</p>
+    <p class="lesson-date">{{ date }}</p>
+    <div class="link-wrapper">
+      <a target="_blank" :href="playURL || shareURL" class="play">Play</a>
+      <a :href="downloadURL" class="download">Download</a>
     </div>
   </li>
 </template>
@@ -21,6 +22,56 @@ export default {
     playURL: {
       type: String,
     },
+    downloadURL: {
+      type: String,
+    },
+    shareURL: {
+      type: String,
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.lr__element-wrapper {
+  border-radius: 0.25rem;
+  border: 0.5px dotted var(--secondary-color);
+}
+.lesson-topic {
+  margin: 0.5rem;
+  text-align: left;
+  font-weight: bold;
+}
+.lesson-date {
+  margin: 0 0.5rem;
+  text-align: left;
+}
+.link-wrapper {
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  margin: 0.5rem;
+}
+.download {
+  color: var(--background-color);
+  font-weight: bold;
+  background-color: var(--primary-color);
+  border: 2.5px solid transparent;
+  border-radius: 0.25rem;
+  padding: 0.3rem 0.7rem;
+  font-family: "Open Sans", sans-serif;
+  font-size: 18px;
+  line-height: 1.5rem;
+}
+.play {
+  color: var(--background-color);
+  font-weight: bold;
+  background-color: var(--secondary-color);
+  border: 2.5px solid transparent;
+  border-radius: 0.25rem;
+  padding: 0.3rem 0.7rem;
+  font-family: "Open Sans", sans-serif;
+  font-size: 18px;
+  line-height: 1.5rem;
+}
+</style>
--- src/components/LRRecordingList/LRRecordingList.vue
+++ src/components/LRRecordingList/LRRecordingList.vue
@@ -1,12 +1,16 @@
 <template>
   <section class="lr__recordig-section">
+    <h2>Lesson Recordings</h2>
+    <div class="filter-wrapper"></div>
     <ul class="lr__recording-list">
       <LRListElement
         v-for="recording in lrRecordingsArray"
         :key="recording.recordingKey"
         :date="recording.recordingData.date"
         :topic="recording.recordingData.topic"
-        :playURL="recording.recordingData['recording-files-play-url']"
+        :playURL="recording.recordingData.recordingFilesPlayUrl"
+        :downloadURL="recording.recordingData.recordingFilesDownloadUrl"
+        :shareURL="recording.recordingData.recordingFilesPlayUrl"
         v-bind="recording"
       />
     </ul>
@@ -31,15 +35,49 @@ export default {
         collection(firestore, "zoom-recordings")
       );
       querySnapshot.forEach((doc) => {
+        let date = new Date(doc.data().date);
         this.lrRecordingsArray.push({
           recordingKey: doc.id,
-          recordingData: doc.data(),
+          recordingData: {
+            date: date.toDateString(),
+            recordingFilesDownloadUrl:
+              doc.data()["recording-files-download-url"] ||
+              doc.data().recordingFilesDownloadUrl,
+            recordingFilesPlayUrl:
+              doc.data()["recording-files-play-url"] ||
+              doc.data().recordingFilesPlayUrl,
+            shareUrl: doc.data()["share-url"] || doc.data().shareUrl,
+            topic: doc.data().topic,
+            uuid: doc.data().uuid,
+            videoFilesDownloadUrl:
+              doc.data()["video-files-download-url"] ||
+              doc.data().videoFilesDownloadUrl,
+          },
         });
       });
+      console.log(this.lrRecordingsArray);
     },
   },
   async mounted() {
-    this.getAllRecordings();
+    await this.getAllRecordings();
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.lr__recording-list {
+  list-style-type: none;
+  display: grid;
+  gap: 0.5rem;
+}
+.lr__recording-list > li {
+  display: grid;
+}
+
+.lr__recording-list > li:nth-child(1n + 1) {
+  background: var(--background-color);
+}
+.lr__recording-list > li:nth-child(2n + 2) {
+  background: var(--light-grey);
+}
+</style>
